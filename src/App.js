435399275388179cbbc10eed491e75996ef7e4c8
JaimeGoB <josba93@gmail.com>
2020-08-17 11:07:54 -0500

    Is raining update implemented

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       low: "",
       weather: "",
       icon: "",
-      cityName: ""
+      cityName: "",
+      isRaining: ""
 
     }
 
@@ -21,8 +22,39 @@ class App extends Component {
   }
 
   componentDidMount() {
+
+    //The default city value for getting weather is Dallas
+    this.getCityWeather("Dallas");
+
+    /*Modal Button effect after clicking it.*/
+    var elems = document.querySelectorAll('.modal');
+    var instances = window.M.Modal.init(elems);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+
+    if (this.state.weather !== prevState.weather) {
+      const isRaining = this.state.weather.includes("rain");
+      if (isRaining) {
+        this.setState({
+          isRaining: "It is raining"
+        })
+      }
+    }
+  }
+
+  searchCity = (e) => {
+    e.preventDefault();
+    //Getting the value from input text from form and storing into city
+    const city = document.getElementById('city').value;
+
+    this.getCityWeather(city);
+  }
+
+  getCityWeather = (city) => {
+
     /*These will be used for API */
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`
     //get request based on url then we will run code
     //inside of anon function
     axios.get(url).then((resp) => {
@@ -35,12 +67,8 @@ class App extends Component {
         cityName: resp.data.name
       })
     })
-
-
-    /*Modal Button effect after clicking it.*/
-    var elems = document.querySelectorAll('.modal');
-    var instances = window.M.Modal.init(elems);
   }
+
 
   render() {
     //Icon lists are in: https://openweathermap.org/weather-conditions
@@ -48,11 +76,25 @@ class App extends Component {
 
     return (
       <div className="App">
+        <div className="row">
+          <div className="col s6 offset-s3">
+            <h1> Weather</h1>
+            <h1> {this.state.isRaining}</h1>
 
-        <h1> Weather</h1>
+
+            {/*This will trigger searchCity function when user submits*/}
+            <form onSubmit={this.searchCity}>
+              <input type="text" id="city" placeholder="Enter a city name"></input>
+            </form>
+
+          </div>
+        </div>
+
 
         {/*Modal Button*/}
         <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Weather Details</a>
+
+        {/*Modal Button*/}
         <div id="modal1" className="modal">
           <div className="modal-content">
             <h4>{this.state.cityName}</h4>
